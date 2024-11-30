a44c8652c469231cbd2d1b9f663a93874693dd67
Akshat Barve <118174073+barveakshat@users.noreply.github.com>
2024-11-30 22:03:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (21.12%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,7 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
+    // using Iterative approach
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode prev = head; 
+        ListNode curr = head.next;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+
+            //update
+            prev = curr;
+            curr = nextNode;
+        }
+        head.next = null;
+        head = prev;
+
+        return head;
+    }
+
+    // Using Recursive approach
+    /* 
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null){
             return head;
@@ -19,4 +43,6 @@ class Solution {
 
         return newHead;
     }
+    */
+    
 }
