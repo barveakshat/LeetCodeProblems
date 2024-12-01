b514f42b9164e49aa0f2a4bbad29448233bbd2b7
Akshat Barve <118174073+barveakshat@users.noreply.github.com>
2024-12-01 19:05:57 +0530

    Time: 1 ms (99.9%), Space: 49.3 MB (5.68%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,48 +9,23 @@
  *     }
  * }
  */
-
- // This is only Better solution
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode dummy1 = headA;
-        ListNode dummy2 = headB;
-        // counting size of both LL
-        int LL1 = 0 , LL2 = 0;
-        while(dummy1 != null){
-            LL1++;
-            dummy1 = dummy1.next;
+        ListNode d1 = headA;
+        ListNode d2 = headB;
+        if(headA == null || headB == null) return null;
+        while(d1 != d2){
+            d1 = d1 == null ? headB : d1.next;
+            d2 = d2 == null ? headA : d2.next;
         }
-        while(dummy2 != null){
-            LL2++;
-            dummy2 = dummy2.next;
-        }
-        dummy1 = headA;
-        dummy2 = headB;
-        if(LL2>LL1){
-            for(int i=0; i<(LL2-LL1); i++){
-                dummy2 = dummy2.next;
-            }
-            while(dummy1 != null && dummy2 != null){
-                if(dummy1 == dummy2){
-                    return dummy1;
-                }
-                dummy1 = dummy1.next;
-                dummy2 = dummy2.next;
-            }
-        }
-        else{
-            for(int i=0; i<(LL1-LL2); i++){
-                dummy1 = dummy1.next;
-            }
-            while(dummy1 != null && dummy2 != null){
-                if(dummy1 == dummy2){
-                    return dummy1;
-                }
-                dummy1 = dummy1.next;
-                dummy2 = dummy2.next;
-            }
-        }
-        return null;
+        return d1;
+
+        
     }
 }
+/*
+Created two dummy nodes
+Then, traverse them through the respective LLs, whenever a dummy node reaches end of its LL, reset it to the head of another LL
+In this way the difference in length of both LLs will be covered
+Then, both dummy nodes will be traversed ahead until they become equal (i.e. reach intersection point)
+*/
