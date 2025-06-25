6b0daa451276a8b994fdb5da55feeb3f657085db
Akshat Barve <118174073+barveakshat@users.noreply.github.com>
2025-06-25 14:41:53 +0530

    Time: 7 ms (64.27%), Space: 44.5 MB (92.71%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,18 +9,23 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //Base Case:
-        //1. If root is null return null
-        //2. If root is equal to p or q, return that root
-        if(root == null || root == p || root == q) return root;
-        //Traverse Left side
-        TreeNode leftN = lowestCommonAncestor(root.left, p, q);
-        //Traverse Right side
-        TreeNode rightN = lowestCommonAncestor(root.right, p, q);
-        //Case 1: If both leftN and rightN are not null, means we got our p and q, so return the root where we got both p and q
-        if(leftN != null && rightN != null) return root;
-        //Case 2: If LeftN is null and rightN is not null, then return rightN
-        //If rightN is null and leftN is not null, then return leftN
-        return leftN == null ? rightN : leftN;
+        if(root == null) return null;
+
+        if(root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q); 
+
+        if(left != null && right != null){
+            return root;
+        }
+        
+        if(left == null){
+            return right;
+        }
+        else{
+            return left;
+        }
     }
 }
